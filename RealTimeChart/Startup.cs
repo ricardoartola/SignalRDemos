da5f746b27137f75691718296176e1e0aea37b14
Ricardo Artola <rartola@outlook.com>
2021-03-10 11:04:44 -0500

    Add project files.

---
+++ RealTimeChart/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RealTimeChart.Startup))]
+namespace RealTimeChart
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
